66f5b799d98a1f781e9256a4f4f194c4e0835ee3
Adem ÇAKMAK <adm.cakmak67@gmail.com>
2023-02-05 14:10:01 +0300

    initial Spring Boot Project for Java 17

---
+++ src/main/java/com/cakmak/SpringBootJava17/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cakmak.SpringBootJava17;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootJava17Application.class);
+	}
+
+}
---
+++ src/main/java/com/cakmak/SpringBootJava17/SpringBootJava17Application.java
@@ -0,0 +1,13 @@
+package com.cakmak.SpringBootJava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJava17Application.class, args);
+	}
+
+}
